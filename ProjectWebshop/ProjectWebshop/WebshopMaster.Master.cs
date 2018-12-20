317e73b0a05b5c19818fd700f523d13514d6ce8b
Colin <colindillier@icloud.com>
2018-12-20 12:55:30 +0100

    Dateien vom sharepoint hinzugefügt

--- ProjectWebshop/ProjectWebshop/WebshopMaster.Master.cs
+++ ProjectWebshop/ProjectWebshop/WebshopMaster.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace ProjectWebshop
 {
-    public partial class Overview : System.Web.UI.Page
+    public partial class WebshopMaster : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
---
+++ ProjectWebshop/ProjectWebshop/WebshopMaster.Master.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace ProjectWebshop {
+    
+    
+    public partial class WebshopMaster {
+        
+        /// <summary>
+        /// head-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// form1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Main-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Main;
+    }
+}
